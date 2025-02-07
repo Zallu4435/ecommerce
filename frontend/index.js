7bf73a4d40b9d6aed765c99421c1f0fa9a355fff
Muhammed Nazal <muhammednazal28@gmail.com>
2025-02-07 22:10:14 +0530

    last

---
+++ frontend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// Serve the React build folder
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`);
+});
