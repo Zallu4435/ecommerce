c46849612bc3e92addd2e88348b79bd19cdb58ff
Zallu <muhammednazal28@gmail.com>
2025-11-11 13:04:36 +0530

    cloudinary bug fixed

--- frontend/src/api/cloudinaryConfig.js
+++ frontend/src/api/cloudinaryConfig.js
@@ -1,7 +1,7 @@
 import { Cloudinary } from 'cloudinary-core';
 
 const cloudinaryInstance = new Cloudinary({
-    cloud_name: 'dbwdttzv4',
+    cloud_name: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || 'your_cloudinary_cloud_name',
     secure: true
   });
   
